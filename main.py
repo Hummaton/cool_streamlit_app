eab795e3319d7ede119b00f80b46b5f2d800c763
Harjot Gill <harjotgill2025@gmail.com>
2024-02-06 09:17:24 -0800

    Imported background image via link. Added user input for name and fav. animal. Created an initial sidebar

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-#Write you name here!!!!!!!!!!!!!!!!!
-
+# Harjot Gill 
 import streamlit as st
 import base64
 import pandas as pd
 from matplotlib import pyplot as plt
 
-#Editing a default Streamlit theme via inspect element/markdowns, (removing header)
+#Background image and button coloring 
+page_styling = """ 
+    <style>
+    [data-testid="stAppViewContainer"] {
+    background-image: url("https://lh3.googleusercontent.com/fife/AGXqzDlKSA5a1hW-ljDjV1v-fHa_U-3EVvIrGhvdQkl4bxxIXLzPDMLkSGuv5_UbD8IP241VNaVGJywDd55fgn-DgkjYYJDnYBNvsOiql8sJ1IeCe2eLEOnbStyZaAakJulIqlEhZ5wzsMlGJS0Ing7XliTc4s-vDEPYDM7CBHWcWo2blYTnrngPI2chasWk3LKm6bkjUJg4VsMwoRZZ2qe4F5u8QPhtOozJX_wkWQRd6LDcaIUJ8AG9Y_g8UQ9s_f_JxNjDHTQd3O3P28IMl9jjpfglAPCiV_m0V9aEVJL6SJVSwSBBCkNTJYdeXSY3Q5CuqplMikGr_F4DqDYyRdZfZgOg4feCC6tRPdlZLZqwhusVkGGjLJ-ygZmqS1UrRGpiP2lIhwK0vPZAHF6MhOfZWSqSrO69aE5D2Pk6qVf-g4CL_abFqt64clHQgEgmKqnELEUTRAzGEjYx3kXcKXPAnwUWGZE3oXH43n1QoiiiCQ7mGYi3rTZ-iIa5KQ5-mSqclEw50iCC5S5zcopCQQSt4VJJ7M0JU3eN-z33HNLEbhzE8ybYDRS8eiOW_rXK7yrv9tISokgpLcmkgib6QAfFSGBDmgG3kR95Xkfz-VFUkjKumZWPSm9aMX55ucqGXGGLUPfGvFNRjhGCFcRIGpbvwbKDaxnODLTxkMewnK1QE5h44Rhm2iDzlEDzRYEoOoa8Hsh2J5NZoAvv1wYVeSQIQ-sP4m8Pj5hMzq8BjFG4eXpgMdR1xS7BgwHMcwswU9N8zPs3Pfus9m0F9MydSbP-thKhfBcxKlapvtxpe0GVy-gcoIuAhw-NDYe2eOTqNYonk3JlGIx6nT0jmRr3E5EdaWuSEq3iyzr8kJMVk33SU8xwsF0tUUW66kUYZqtdYUyXePktFgd2cJS0PdZcvaXQ7W0HyChRXisRePLjcacDUJmEF5_ELVmwMjFDPDiTlokJt2SgFdsuRIWy-EagPI83xRyGOCh25CWkrQalAmxCr264nNaqvNMwLM-NRiudFneryhwK_y6u9fpTpUbMpFvxKK_j8G0b_UZ2mnfz5lo-XQur7ID1CVus3CZ2RqnxYaD3pzQYSCu__ItdbfbQB_jB2qN5wZmIYattyGtcGQxDQw7gv4oiroV4SYHwf74-0_oOs7KkwcWTZsEBkUigoJygcVp3dXOgjt-dP0Ir65Sbwgppc3fxl9L1rlQfcH1oeN3EuAgOxMFhPE4EAlytZXdTE89tR7uUeP7VFqA2Xkjjj90IUZgueBgY9djgCKxDrhJPvc2P3C8SBAzfowTFr3uKsPQppJFbBTO4z5sexOR3muSP6tj_F0RVKz50DdpN8DYb5cg0wI97EvQoC5P9gdVAE0nq0k6_UgaD87Ypo-zmjNksAEkX6kwe9rG88JFmP7X9qtENIo7bX5v6iGetvwBvRfIc97gIAlbVLC4N0G9hzQGn1PoPYjwoJpRoCKYhbBgYZS3ov3H4cz64Eo_rj8WTP8RZmiejYn6-6brRYQFiAd7lebWThHqqCer-3RFkMqu68-k-orWpql4L32bd-iT7AXt-wNMMW4MT2eeVwhmAnfzK-QMb-xorcve7dOVjncNNebNMuLXDxI6dJ5mGBqs=w958-h893");
+    background-size: cover;
+    }
+
+    # Figure this out!!!! 
+
+    .st-ax input {
+    background-color: white;
+    }
+
+    .stSelectbox div[data-baseweb="select"] > div:first-child {
+            background-color: white;
+            border-color: #2d408d;
+        }
+
+    .st-bb {
+        color: black;
+        background: snow;
+
+        
+    }
+
+}
+    </style>
+    """
+st.markdown(page_styling, unsafe_allow_html=True)
+
+
+
+#Removing header)
 remove_header = """
 <style>
 .stDeployButton
@@ -16,6 +48,7 @@ remove_header = """
     {
         visibility: hidden;
     }
+
 [data-testid="stHeader"] {
     background-color: rgba(0, 0, 0, 0.0);
 }
@@ -23,21 +56,31 @@ remove_header = """
 """
 st.markdown(remove_header, unsafe_allow_html=True)
 
-#Background image 
-page_bg_img = """
-    <style>
-    [data-testid="stAppViewContainer"] {
-    background-image: url("https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w300/2023/09/instagram-image-size.jpg");
-    background-size: cover;
-    }
-    </style>
-    """
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
-#Title of the page
+# Title of the page
 title = "EcoCAR Form"
-st.markdown(f"<h1 style='text-align: center;'>{title}</h1>", unsafe_allow_html=True)
-st.sidebar.title("Centered Sidebar")
+st.markdown(f"<center><h1>{title}</h1></center>", unsafe_allow_html=True)
+
+with st.sidebar:
+    st.markdown("<center><h1>Centered Sidebar</h1></center>", unsafe_allow_html=True)
+
+
+
+col1, col2 = st.columns(2)
+col1.text_input("First Name")
+col2.text_input("Last Name") 
+
+
+select = st.selectbox("Pick your favorite animal", options=["Select an animal", "dog", "cat", "bird", "fish", "rabbit"])
+submit_button = st.button("Submit")
+
+
+
+
+
+
+
+
+
 
 
 
